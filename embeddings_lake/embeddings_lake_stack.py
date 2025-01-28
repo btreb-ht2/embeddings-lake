d5dacbad9d5a4b92c5e5176925cd90601f8a9811
bill <ht2.ab8x954@gmail.com>
2025-01-28 13:14:49 -0500

    (#5) add embeddings_lake internal code to lambda

--- embeddings_lake/embeddings_lake_stack.py
+++ embeddings_lake/embeddings_lake_stack.py
@@ -2,7 +2,8 @@ from aws_cdk import (
     # Duration,
     Stack,
     aws_s3 as s3,
-    aws_lambda as lambda_
+    aws_lambda as lambda_,
+    BundlingOptions
 )
 from constructs import Construct
 
@@ -14,18 +15,49 @@ class EmbeddingsLakeStack(Stack):
         bucket = s3.Bucket(
             self, "EmbeddingsBucket",
         )
-        # The code that defines your stack goes here
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "EmbeddingsLakeQueue",
-        #     visibility_timeout=Duration.seconds(300),
+        # layer_bundling_command = (
+        #     "pip install -r requirements.txt "
+        #     "-t /asset-output/python && "
+        #     "find /asset-output/python -type d -name '__pycache__' -exec rm -rf {} + && "
+        #     "cp -au . /asset-output/python"
         # )
+
+        # lambda_layer_pydantic = lambda_.LayerVersion(
+        #     self,
+        #     "numpyLambdaLayer",
+        #     compatible_runtimes=[lambda_.Runtime.PYTHON_3_10],
+        #     code=lambda_.Code.from_asset(
+        #         "embeddings_lake/assets/lambda/layers/pydantic",
+        #         bundling=BundlingOptions(
+        #             image=lambda_.Runtime.PYTHON_3_10.bundling_image,
+        #             command=[
+        #                 "bash",
+        #                 "-c",
+        #                 layer_bundling_command,
+        #             ]
+        #         )
+        #     )
+        # )
+
+        # https://www.youtube.com/watch?v=jyuZDkiHe2Q
+        lambda_layer_pydantic = lambda_.LayerVersion(
+            self,
+            "pydanticLambdaLayer",
+            code=lambda_.Code.from_asset("embeddings_lake/assets/lambda/layers/pydantic/lambda-layer-pydantic.zip"),
+        )
+
+        lambda_layer_pandas = lambda_.LayerVersion.from_layer_version_arn(
+            self,
+            "pandasLambdaLayer",
+            layer_version_arn="arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python310:23"
+        )
 
         function = lambda_.Function(
             self,
             "EmbeddingsFunction",
             runtime=lambda_.Runtime.PYTHON_3_10,
             handler="index.lambda_handler",
-            code=lambda_.Code.from_asset("embeddings_lake/assets/lambda/embedder")
+            code=lambda_.Code.from_asset("embeddings_lake/assets/lambda/embedder"),
+            layers=[lambda_layer_pandas, lambda_layer_pydantic]
         )
