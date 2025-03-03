2286e8462d0fdfedc1e4e394f99a20b8433cca0e
bill <ht2.ab8x954@gmail.com>
2025-03-02 21:42:45 -0500

    (#49) query api

--- embeddings_lake/assets/lambda/adjacent/index.py
+++ embeddings_lake/assets/lambda/adjacent/index.py
@@ -1,3 +1,11 @@
+import logging
+
+
+logger = logging.getLogger()
+logger.setLevel(level=logging.INFO)
+
+
+
 def get_adjacent_segments(base_hash, num_shards, radius=1):
         # Added radius to include closest bucket as well
         segment_indices = []
@@ -9,18 +17,27 @@ def get_adjacent_segments(base_hash, num_shards, radius=1):
 
 def lambda_handler(event, context):
 
-    print(event)
+    logger.info(event)
 
     n_results: int = 4,
-    radius: int = 2
-    embedding = event["Payload"]["embedding"]
-    embedding_hash_index = event["Payload"]["embedding_hash_index"]
-    num_shards = event["Payload"]["num_shards"]
+    radius: int = 0
+    lake_name = event['Payload']['lake_name']
+    embedding = event['Payload']['embedding']
+    embedding = event['Payload']['embedding']
+    embedding_hash_index = event['Payload']['segment_index']
+    num_shards = event["Payload"]['num_shards']
 
 
     segment_indices_to_search = get_adjacent_segments(embedding_hash_index, num_shards, radius)
 
-    return { 
-        'segmentIndices': list(map(str, segment_indices_to_search)),
+    segments_as_strings = list(map(str, segment_indices_to_search))
+
+    results = { 
+        'segmentIndices': segments_as_strings,
         'embedding': embedding,
+        'lakeName': lake_name,
     }
+
+    logger.info(results)
+
+    return results
